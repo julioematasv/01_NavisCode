df8a0a5ffd6f42f42e700f013ad1190e6a5f8bb8
Julio Mata <julioematasv@gmail.com>
2023-03-30 10:49:15 +0200

    Creados archivos de ejercio 3

---
+++ bootcampJS/modulos/1_basesJavascript/ejercicio_03/mainEjercicio3.js
@@ -0,0 +1,13 @@
+let numero1 = 15;
+let numero2 = 5;
+let resultado1, resultado2, resultado3, resultado4;
+
+console.log("El resultado de la suma es " + (resultado1 = numero1 + numero2));
+console.log("El resultado de la resta es " + (resultado2 = numero1 - numero2));
+console.log("El resultado de la multiplicación es " + (resultado3 = numero1 * numero2));
+console.log("El resultado de la división es " + (resultado4 = numero1 / numero2));
+
+document.write("El resultado de la suma es " + (resultado1 = numero1 + numero2) + "<br>");
+document.write("El resultado de la resta es " + (resultado2 = numero1 - numero2) + "<br>");
+document.write("El resultado de la multiplicación es " + (resultado3 = numero1 * numero2) + "<br>");
+document.write("El resultado de la división es " + (resultado4 = numero1 / numero2) + "<br>");
