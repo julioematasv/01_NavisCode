daa3a8aa15c1ba195bdad27bd782f77a9b48b3e6
Julio Mata <julioematasv@gmail.com>
2023-03-30 13:39:19 +0200

    Añadidos archivos de ejercicio 5

---
+++ bootcampJS/modulos/1_basesJavascript/ejercicio_05/mainEjercicio5.js
@@ -0,0 +1,14 @@
+let listaPokemon = ["Bulbasaur", "Ivysaur", "Venusaur"];
+
+document.write("Este es el primer Pokemon: " + listaPokemon[0] +"<br>");
+document.write("Este es el segundo Pokemon: " + listaPokemon[1] +"<br>");
+document.write("Este es el tercer Pokemon: " + listaPokemon[2] +"<br>");
+console.log("Este es el listado de Pokemons: " + listaPokemon);
+document.write("<br><br>Este es el listado de Pokemons: " + listaPokemon);
+
+document.write("<br><br><br> Listado por un bucle for in:<br>");
+for(let pokemon in listaPokemon){
+    console.log(listaPokemon[pokemon]);
+
+    document.write(listaPokemon[pokemon] + "<br>")
+}
